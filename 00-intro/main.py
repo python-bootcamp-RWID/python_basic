f8187dd864a66d42702cc9095e6e343a444c1b7f
danil syah <danilsyaharihardjo@gmail.com>
2020-06-11 13:16:30 +0700

    edit file

---
+++ 00-intro/main.py
@@ -0,0 +1,21 @@
+# KONSTRUKSI DASAR PYTHON
+# SEQUENTIAL : Eksekusi ber-urutan
+
+print('+'*100)
+print('Hello World')
+print('Danil Syah Arihardjo')
+print('Tanggal 11 Juni 2020')
+
+# PERCABANGAN : Eksekusi terpilih
+ingin_cepat = True
+if ingin_cepat:
+    print('jalan lurus aja')
+else:
+    print('Jalan lain')
+
+
+# PERULANGAN
+jumlah_anak = 4
+for index_anak in range(1, jumlah_anak+1):
+    print(f'halo anak ke- {index_anak}')
+
